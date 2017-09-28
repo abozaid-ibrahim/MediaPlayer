e83e3126c7c6f492943285c57e2fc62f3a9b6990
abuzeid-timore <abuzeidtimore@gmail.com>
2017-09-28 17:14:45 +0200

    Initial Commit

---
+++ MediaPlayer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MediaPlayer
+//
+//  Created by abuzeid on 9/28/17.
+//  Copyright © 2017 aone. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
